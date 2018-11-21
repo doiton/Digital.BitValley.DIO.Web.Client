2a2556319d36beea2224956309711b53518f777c
Levon Tsverinyan <xakerwoolf@mail.ru>
2018-11-22 01:40:08 +0400

    1

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,20 +1,20 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './Search.css';
 
 
-class Search extends Component{
-    render(){
-        return(
-         
-  <section className="search-component-container"> 
+class Search extends Component {
+    render() {
+        return (
+
+            <section className="search-component-container">
                 <div className="item1" >
-                    <input type="text" className="search-component-input"  placeholder={this.props.placeholder} />
-                 </div>   
-                 <div className="search-component-item" >
+                    <input type="text" className="search-component-input" placeholder={this.props.placeholder} />
+                </div>
+                <div className="search-component-item" >
                     <button type="submit" className="search-component-button search-component-button-default">{this.props.buttonName}</button>
-                    </div>
-                
-                </section>
+                </div>
+
+            </section>
         );
     }
 }
